01e8191886b0dc0e69ce5257916c1e71f3a9cc33
sashastewart-28 <43047896+sashastewart-28@users.noreply.github.com>
2018-09-10 09:59:36 +0530

    Update largestpalindrome.java

--- largestpalindrome.java
+++ largestpalindrome.java
@@ -1,4 +1,4 @@
-//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 Ã— 99.
 //Find the largest palindrome made from the product of two 3-digit numbers.
 //EULER PROJECT 04
 public class largestpalindrome 
@@ -8,9 +8,9 @@ public class largestpalindrome
 	{
 		// TODO Auto-generated method stub
 		int i,j,p,max=0,dup=0,d=0,rev=0;
-		for(i=100;i<=999;i++)
+		for(i=999;i>=100;i--)
 		{
-			for(j=100;j<=999;j++)
+			for(j=i;j>=100;j--)
 			{
 				p=i*j;
 				dup=p;
