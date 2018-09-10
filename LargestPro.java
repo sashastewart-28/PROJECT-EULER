b5a362c23b3733d7da78d85fdccf73252df890eb
sashastewart-28 <43047896+sashastewart-28@users.noreply.github.com>
2018-09-10 10:09:58 +0530

    Update and rename largestpro.java to LargestPro.java

--- LargestPro.java
+++ LargestPro.java
@@ -1,7 +1,7 @@
 //The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
 //Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 //EULER PROJECT 08
-public class largestpro 
+public class LargestPro 
 {
 
 	public static void main(String[] args) 
