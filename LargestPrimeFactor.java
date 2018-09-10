74f67a318f8e8627802e95c7b5a791977f6e19b0
sashastewart-28 <43047896+sashastewart-28@users.noreply.github.com>
2018-09-10 10:26:48 +0530

    Update and rename largestprimefactor.java to LargestPrimeFactor.java

--- LargestPrimeFactor.java
+++ LargestPrimeFactor.java
@@ -1,7 +1,7 @@
 //The prime factors of 13195 are 5, 7, 13 and 29.
 //What is the largest prime factor of the number 600851475143 ?
 //EULER PROJECT 03
-public class largestprimefactor 
+public class LargestPrimeFactor 
 {
 
 	public static void main(String[] args) 
